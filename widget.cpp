d6bc920a3f66572770f7e19059c362b9b0dcdd25
zzttzzmyswy <2775859312@qq.com>
2023-01-16 09:01:05 +0800

    实现简易的绘制功能和键盘空格键捕获功能

--- widget.cpp
+++ widget.cpp
@@ -2,14 +2,87 @@
 #include "ui_widget.h"
 
 Widget::Widget(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::Widget)
-{
-    ui->setupUi(this);
+	: QWidget(parent)
+	, ui(new Ui::Widget) {
+	ui->setupUi(this);
+	plane1 = new PlanetInf(QString("test"), 0.5, 0, 0, 1.63);
+	plane1->WorkInt();
+	pixmap.fill(Qt::white);
+	ui->label->setPixmap(pixmap);
+	painter.begin(&pixmap);
+	//设置抗锯齿，反走样。如果为false会有锯齿
+	painter.setRenderHint(QPainter::Antialiasing, true);
+	pen.setWidth(2);
+	pen.setColor(Qt::black);
+	painter.setPen(pen);
+	line.setLine(400, 0, 400, 800);
+	painter.drawLine(line);
+	line.setLine(0, 400, 800, 400);
+	painter.drawLine(line);
+	painter.end();
+	ui->label->setPixmap(pixmap);
+	Timer = new QTimer(this);
+	connect(Timer, &QTimer::timeout, [ = ]() {
+		double x, y;
+		painter.begin(&pixmap);
+		pen.setColor(Qt::red);
+		painter.setPen(pen);
+		plane1->Work(x, y);
+		px = 400 + 200 * x;
+		py = 400 + 200 * y;
+		qDebug() << px << " " << py;
+		painter.drawPoint(px, py);
+		painter.end();
+		ui->label->setPixmap(pixmap);
+	});
+	Timer->start(1);
 }
 
-Widget::~Widget()
-{
-    delete ui;
+Widget::~Widget() {
+	delete ui;
+}
+
+void Widget::keyPressEvent(QKeyEvent *event) {
+	int key = event->key();
+	painter.begin(&pixmap);
+	if (key == Qt::Key_Space) {
+		pixmap.fill(Qt::white);
+		pen.setWidth(2);
+		pen.setColor(Qt::black);
+		painter.setPen(pen);
+		line.setLine(400, 0, 400, 800);
+		painter.drawLine(line);
+		line.setLine(0, 400, 800, 400);
+		painter.drawLine(line);
+	}
+	painter.end();
+}
+
+void Widget::paintEvent(QPaintEvent *event) {
+	//  painter.begin(this);
+	//  switch (runState) {
+	//  case 0:
+	//      //设置抗锯齿，反走样。如果为false会有锯齿
+	//      painter.setRenderHint(QPainter::Antialiasing, true);
+	//      pen.setWidth(2);
+	//      pen.setColor(Qt::black);
+	//      painter.setPen(pen);
+	//      line.setLine(400, 0, 400, 800);
+	//      painter.drawLine(line);
+	//      line.setLine(0, 400, 800, 400);
+	//      painter.drawLine(line);
+	//      runState = 1;
+	//      break;
+	//  case 1:
+	//      qDebug() << px << " " << py;
+	//      pen.setWidth(2);
+	//      pen.setColor(Qt::black);
+	//      painter.setPen(pen);
+	//      painter.drawPoint(px, py);
+	//      break;
+	//  default:
+	//      break;
+	//  }
+	//  painter.end();
 }
 
--- widget.h
+++ widget.h
@@ -2,20 +2,99 @@
 #define WIDGET_H
 
 #include <QWidget>
+/* 加入画笔支持 */
+#include <QPainter>
+#include <QPen>
+#include <QColor>
+#include <QRgb>
+#include <QLine>
+#include <QLineF>
+#include <QImage>
+#include <QKeyEvent>
+#include <QPaintEvent>
+#include <QString>
+#include <QTimer>
+#include <QDebug>
+#include <QPixmap>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class Widget; }
+namespace Ui {
+class Widget;
+}
 QT_END_NAMESPACE
 
-class Widget : public QWidget
-{
-    Q_OBJECT
+#define MY_DT (0.001) /* 时间步长 */
+#define MY_SUN_M (1) /* 太阳质量 */
+#define MY_G (1) /* 万有引力常熟 */
 
-public:
-    Widget(QWidget *parent = nullptr);
-    ~Widget();
-
-private:
-    Ui::Widget *ui;
+class PlanetInf {
+  public:
+	PlanetInf(QString nameIn, double x0In, double y0In,
+		double vx0In, double vy0In)
+		: name(nameIn), x0(x0In), y0(y0In), vx0(vx0In), vy0(vy0In) {
+		WorkClear();
+	}
+	void WorkClear(void) {
+		x = x0;
+		x1 = 0;
+		y = y0;
+		y1 = 0;
+	}
+	void WorkInt() {
+		x = x0;
+		y = y0;
+		x1 = x0 + MY_DT * vx0;
+		y1 = y0 + MY_DT * vy0;
+	}
+	void Work(double &xout, double &yout) {
+		double xOld = x1, yOld = y1;
+		r = sqrt((x) * (x) + (y) * (y));
+		x1 = -MY_G * MY_SUN_M * (x) / pow(r, 3) * pow(MY_DT,
+				2) + 2 * (x1) - (x);
+		y1 = -MY_G * MY_SUN_M * (y) / pow(r, 3) * pow(MY_DT,
+				2) + 2 * (y1) - (y);
+		x = xOld;
+		y = yOld;
+		xout = x1;
+		yout = y1;
+	}
+  private:
+	QString name;
+	double x0;
+	double y0;
+	double vx0;
+	double vy0;
+	double x, x1;
+	double y, y1;
+	double r;
 };
+
+class Widget : public QWidget {
+	Q_OBJECT
+
+  public:
+	Widget(QWidget *parent = nullptr);
+	~Widget();
+	void paintEvent(QPaintEvent *event);
+	void keyPressEvent(QKeyEvent *event);
+
+  private:
+	Ui::Widget *ui;
+	QPainter painter;
+	QPen pen;
+	QLineF line;
+	/* 当前行星编号 */
+	int planetFlag = 0;
+	/* 不同的行星 */
+	PlanetInf *plane1;
+	/* 当前绘制的曲线 */
+	PlanetInf *planeRun;
+	QTimer *Timer;
+	/* 绘图状态机 */
+	int runState = 0;
+	int px, py;
+	QPixmap pixmap = QPixmap(800, 800);
+};
+
+
 #endif // WIDGET_H
