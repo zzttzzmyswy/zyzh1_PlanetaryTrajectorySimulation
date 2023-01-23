7a1a0e5dadd828a7805e08490062873b459721b6
zzttzzmyswy <2775859312@qq.com>
2023-01-23 19:48:54 +0800

    增加全部行星共同仿真的功能

--- widget.cpp
+++ widget.cpp
@@ -1,6 +1,12 @@
 #include "widget.h"
 #include "ui_widget.h"
 
+#ifndef TARGET_ARCH_ARM
+	#define USER_TD (0.001)
+#else
+	#define USER_TD (0.04)
+#endif
+
 Widget::Widget(QWidget *parent)
 	: QWidget(parent)
 	, ui(new Ui::Widget) {
@@ -9,62 +15,78 @@ Widget::Widget(QWidget *parent)
 	planeRun->WorkInt();
 	PixmapInit();
 	Timer = new QTimer(this);
-    /*使用lambda表达式简化函数调用*/
+	/*使用lambda表达式简化函数调用*/
 	connect(Timer, &QTimer::timeout, [ = ]() {
 		double x, y, r, r0, a;
-        QString strShow1, strShow2, strShow3;
+		QString strShow1, strShow2, strShow3;
 		painter.begin(&pixmap);
-		pen.setColor(Qt::red);
-		painter.setPen(pen);
-		/* 在不损失仿真精度的前提下一定程度地提高仿真速度 */
-#ifndef TARGET_ARCH_ARM
-        for (int i = 0; i < 1000; i++)
-#else
-        for (int i = 0; i < 100; i++)
-#endif
-			planeRun->Work(x, y);
-		/* 对输出的坐标点极坐标化，
-		 * 并且将与太阳的距离对数化，
-		 * 以确保水星的轨迹容易辨识*/
-		r0 = sqrt((x) * (x) + (y) * (y));
-		r = qLn(r0 + 1) / qLn(51);
-		a = qAtan2(y, x);
-		/* 将极坐标转为直角坐标系坐标 */
-		x = r * qCos(a);
-		y = r * qSin(a);
-        px = 250 + 240 * x;
-        py = 250 + 240 * y;
-		/* 显示当前仿真点的数据 */
-        strShow1 = QString("距离%1").arg(r0);
-        strShow2 = QString("角度%2°").arg(
-                a / M_PI * 180);
-        strShow3 = QString("坐标(%3,%4)").arg(px - 250).arg(250 - py);
-        ui->label_2->setText(strShow1);
-        ui->label_5->setText(strShow2);
-        ui->label_6->setText(strShow3);
-        /* qDebug() << strShow1 << strShow2 << strShow3; */
-        /* 绘制仿真结果 */
-#ifndef TARGET_ARCH_ARM /* 选择绘制连线还是点,arm平台由于仿真精度低所以绘制连线 */
-		painter.drawPoint(px, py);
-#else
-        if (pxOld == 0) {
-            pxOld = px;
-            pyOld = py;
-        }
-        painter.drawLine(px, py, pxOld, pyOld);
-        pxOld = px;
-        pyOld = py;
-#endif
-		painter.end();
-		/* 开始绘制顶层 */
+		/* 将顶层清空 */
 		pixmapTop.fill(colorTop);
 		painterTop.begin(&pixmapTop);
-		penTop.setColor(Qt::blue);
-		penTop.setWidth(1);
-		painterTop.setPen(penTop);
-		painterTop.setBrush(Qt::blue);
-		painterTop.drawEllipse(px - 5, py - 5, 10, 10);
+		pen.setColor(Qt::red);
+		painter.setPen(pen);
+		for (int i = 0; i <= plantMax; i++) {
+			/* 在不损失仿真精度的前提下一定程度地提高仿真速度 */
+#ifndef TARGET_ARCH_ARM
+			for (int k = 0; k < 1000; k++)
+#else
+			for (int k = 0; k < 100; k++)
+#endif
+				if (planetFlag <= plantMax)
+					planeRun->Work(x, y);
+				else
+					/* 使用USER_TD确保所有行星仿真步长一致 */
+					planes.at(i)->Work(x, y, USER_TD);
+			/* 对输出的坐标点极坐标化，
+			 * 并且将与太阳的距离对数化，
+			 * 以确保水星的轨迹容易辨识*/
+			r0 = sqrt((x) * (x) + (y) * (y));
+			r = qLn(r0 + 1) / qLn(51);
+			a = qAtan2(y, x);
+			/* 将极坐标转为直角坐标系坐标 */
+			x = r * qCos(a);
+			y = r * qSin(a);
+			px = 250 + 240 * x;
+			py = 250 + 240 * y;
+			if (planetFlag <= plantMax) {
+				/* 显示当前仿真点的数据 */
+				strShow1 = QString("距离%1").arg(r0);
+				strShow2 = QString("角度%2°").arg(
+						a / M_PI * 180);
+				strShow3 = QString("坐标(%3,%4)").arg(px - 250).arg(250 - py);
+				ui->label_2->setText(strShow1);
+				ui->label_5->setText(strShow2);
+				ui->label_6->setText(strShow3);
+			}
+			else {
+				ui->label_2->setText(QString("在全部仿真模式下，"));
+				ui->label_5->setText(
+					QString("仿真按照真实时间比例"));
+				ui->label_6->setText(QString("水金地火木土天海冥"));
+			}
+			/* 绘制仿真结果 */
+#ifndef TARGET_ARCH_ARM /* 选择绘制连线还是点,arm平台由于仿真精度低所以绘制连线 */
+			painter.drawPoint(px, py);
+#else
+			if (pxOlds[i] == 0) {
+				pxOlds[i] = px;
+				pyOlds[i] = py;
+			}
+			painter.drawLine(px, py, pxOlds[i], pyOlds[i]);
+			pxOlds[i] = px;
+			pyOlds[i] = py;
+#endif
+			/* 开始绘制顶层 */
+			penTop.setColor(Qt::blue);
+			penTop.setWidth(1);
+			painterTop.setPen(penTop);
+			painterTop.setBrush(Qt::blue);
+			painterTop.drawEllipse(px - 5, py - 5, 10, 10);
+			if (planetFlag <= plantMax)
+				break;
+		}
 		painterTop.end();
+		painter.end();
 		ui->label->setPixmap(pixmap);
 		ui->label_4->setPixmap(pixmapTop);
 	});
@@ -81,7 +103,7 @@ void Widget::keyPressEvent(QKeyEvent *event) {
 	if (key == Qt::Key_Space) {
 		Timer->stop();
 		while (Timer->isActive());
-		planetFlag = planetFlag < plantMax ? planetFlag + 1 : 0;
+		planetFlag = planetFlag < plantMax + 1 ? planetFlag + 1 : 0;
 		PlanetInit();
 		PixmapInit();
 		Timer->start(1);
@@ -100,35 +122,48 @@ void Widget::PixmapInit() {
 	pen.setWidth(2);
 	pen.setColor(Qt::black);
 	painter.setPen(pen);
-    line.setLine(250, 0, 250, 500);
+	line.setLine(250, 0, 250, 500);
 	painter.drawLine(line);
-    line.setLine(0, 250, 500, 250);
+	line.setLine(0, 250, 500, 250);
 	painter.drawLine(line);
-	painter.drawText((const QRectF)(rect), planeRun->name);
+	if (planetFlag <= plantMax)
+		painter.drawText((const QRectF)(rect), planeRun->name);
+	else
+		painter.drawText((const QRectF)(rect), QString("全部"));
 	pen.setColor(Qt::yellow);
 	painter.setPen(pen);
 	painter.setBrush(Qt::yellow);
-    painter.drawEllipse(250 - 10, 250 - 10, 20, 20);
+	painter.drawEllipse(250 - 10, 250 - 10, 20, 20);
 	painter.end();
 	ui->label->setPixmap(pixmap);
 	ui->label_4->setPixmap(pixmapTop);
-    pxOld = 0;
-    pyOld = 0;
 }
 
 /* 初始化行星计算资源 */
 void Widget::PlanetInit() {
-	planeRun = planes.at(planetFlag);
-	planeRun->WorkInt();
+	if (planetFlag <= plantMax) {
+		planeRun = planes.at(planetFlag);
+		planeRun->WorkInt();
+		pxOlds[0] = 0;
+		pyOlds[0] = 0;
+	}
+	else {
+		for (int i = 0; i <= plantMax; i++) {
+			/* 使用USER_TD确保所有行星仿真步长一致 */
+			planes.at(i)->WorkInt(USER_TD);
+			pxOlds[i] = 0;
+			pyOlds[i] = 0;
+		}
+	}
 }
 
 void Widget::on_pushButton_clicked() {
-    /* 切换行星 */
-    Timer->stop();
-    while (Timer->isActive());
-    planetFlag = planetFlag < plantMax ? planetFlag + 1 : 0;
-    PlanetInit();
-    PixmapInit();
-    Timer->start(1);
+	/* 切换行星 */
+	Timer->stop();
+	while (Timer->isActive());
+	planetFlag = (planetFlag < plantMax + 1) ? planetFlag + 1 : 0;
+	PlanetInit();
+	PixmapInit();
+	Timer->start(1);
 }
 
--- widget.h
+++ widget.h
@@ -30,133 +30,137 @@ QT_END_NAMESPACE
 
 class PlanetInf {
   public:
-    /* 初始话行星参数 名字 初始坐标 初始速度 仿真时间步长 */
-    PlanetInf(QString nameIn, double x0In, double y0In,
-        double vx0In, double vy0In, double dtIn)
-        : name(nameIn), x0(x0In), y0(y0In), vx0(vx0In), vy0(vy0In), dt(dtIn) {
-        WorkInt();
-    }
-    /* 为迭代计算初始化参数 */
-    void WorkInt() {
-        x = x0;
-        y = y0;
-        x1 = x0 + dt * vx0;
-        y1 = y0 + dt * vy0;
-    }
-    /* 进行迭代计算 */
-    void Work(double &xout, double &yout) {
-        double xOld = x1, yOld = y1;
-        r = sqrt((x) * (x) + (y) * (y));
-        /* 由行星运动公式差分获得 */
-        x1 = -MY_G * MY_SUN_M * (x) / pow(r, 3) * pow(dt,
-                2) + 2 * (x1) - (x);
-        y1 = -MY_G * MY_SUN_M * (y) / pow(r, 3) * pow(dt,
-                2) + 2 * (y1) - (y);
-        x = xOld;
-        y = yOld;
-        xout = x1;
-        yout = y1;
-#ifdef TARGET_ARCH_ARM /* 用于刷新数据，抵消掉低仿真精度带来的误差 */
-        if (x1 > 0 && abs(y1) < 0.001 && flashFlag == false) {
-            WorkInt();
-            flashFlag = true;
-        }
-        else if (x1 < 0 && abs(y1) < 0.001 && flashFlag == true)
-            flashFlag = false;
+	/* 初始话行星参数 名字 初始坐标 初始速度 仿真时间步长 */
+	PlanetInf(QString nameIn, double x0In, double y0In,
+		double vx0In, double vy0In, double dtIn)
+		: name(nameIn), x0(x0In), y0(y0In), vx0(vx0In), vy0(vy0In), dt0(dtIn) {
+		WorkInt();
+	}
+	/* 为迭代计算初始化参数 */
+	void WorkInt(double dtIn = 0) {
+		dt = dtIn == 0 ? dt0 : dtIn;
+		x = x0;
+		y = y0;
+		x1 = x0 + dt * vx0;
+		y1 = y0 + dt * vy0;
+	}
+	/* 进行迭代计算 */
+	void Work(double &xout, double &yout, double dtIn = 0) {
+		xOld = x1, yOld = y1;
+		dt = dtIn == 0 ? dt0 : dtIn;
+		r = sqrt((x) * (x) + (y) * (y));
+		/* 由行星运动公式差分获得 */
+		x1 = -MY_G * MY_SUN_M * (x) / pow(r, 3) * pow(dt,
+				2) + 2 * (x1) - (x);
+		y1 = -MY_G * MY_SUN_M * (y) / pow(r, 3) * pow(dt,
+				2) + 2 * (y1) - (y);
+		x = xOld;
+		y = yOld;
+		xout = x1;
+		yout = y1;
+#if 1 /* 用于刷新数据，抵消掉低仿真精度带来的误差 */
+		if (x1 > 0 && abs(y1) < 0.001 && flashFlag == false) {
+			WorkInt(dt);
+			flashFlag = true;
+		}
+		else if (x1 < 0 && abs(y1) < 0.001 && flashFlag == true)
+			flashFlag = false;
 #endif
-    }
-    QString name;
+	}
+	QString name;
+	double x, x1, xOld;
+	double y, y1, yOld;
   private:
-    double x0;/* 起始坐标 */
-    double y0;
-    double vx0;/* 其实初速度 */
-    double vy0;
-    double dt;/* 时间步长 */
-    double x, x1;
-    double y, y1;
-    double r;
-    bool flashFlag =
-        false; /* 用于刷新数据，抵消掉低仿真精度带来的误差 */
+	double x0;/* 起始坐标 */
+	double y0;
+	double vx0;/* 其实初速度 */
+	double vy0;
+	double dt0, dt; /* 时间步长 */
+	double r;
+	/* 用于刷新数据，抵消掉低仿真精度带来的误差 */
+	bool flashFlag = false;
 };
 
 class Widget : public QWidget {
-    Q_OBJECT
+	Q_OBJECT
 
   public:
-    Widget(QWidget *parent = nullptr);
-    ~Widget();
-    void keyPressEvent(QKeyEvent *event);
-    void PixmapInit(void);
-    void PlanetInit(void);
+	Widget(QWidget *parent = nullptr);
+	~Widget();
+	void keyPressEvent(QKeyEvent *event);
+	void PixmapInit(void);
+	void PlanetInit(void);
 
   private slots:
-    void on_pushButton_clicked();
+	void on_pushButton_clicked();
 
   private:
-    Ui::Widget *ui;
-    /* 当前行星编号 */
-    int planetFlag = 0;
-    const int plantMax = 8;
+	Ui::Widget *ui;
+	/* 当前行星编号 */
+	int planetFlag = 0;
+	const int plantMax = 8;
 #ifndef TARGET_ARCH_ARM /* 选择不同的仿真时间步长以在不同的平台上流畅运行 */
 	/* 不同的行星 */
 	PlanetInf plane1 = PlanetInf(QString("水星"), 0.466699373,
-            0, 0, 0.0224454181635083, 0.0001);
+			0, 0, 0.0224454181635083, 0.0001);
 	PlanetInf plane2 = PlanetInf(QString("金星"), 0.728230404304,
-            0, 0, 0.0200920831078843, 0.001);
+			0, 0, 0.0200920831078843, 0.0005);
 	PlanetInf plane3 = PlanetInf(QString("地球"), 1.01669516395119,
-            0, 0, 0.0169192315430405, 0.001);
+			0, 0, 0.0169192315430405, 0.001);
 	PlanetInf plane4 = PlanetInf(QString("火星"), 1.66600940556207,
-            0, 0, 0.0126911527706869, 0.001);
+			0, 0, 0.0126911527706869, 0.001);
 	PlanetInf plane5 = PlanetInf(QString("木星"), 5.45889516,
-            0, 0, 0.00718117658914553, 0.005);
+			0, 0, 0.00718117658914553, 0.005);
 	PlanetInf plane6 = PlanetInf(QString("土星"), 10.1240169,
-            0, 0, 0.00525205679561466, 0.01);
+			0, 0, 0.00525205679561466, 0.01);
 	PlanetInf plane7 = PlanetInf(QString("天王星"), 20.1097686104,
-            0, 0, 0.0037464401867908, 0.01);
+			0, 0, 0.0037464401867908, 0.01);
 	PlanetInf plane8 = PlanetInf(QString("海王星"), 30.33094746,
-            0, 0, 0.00311027848501303, 0.02);
+			0, 0, 0.00311027848501303, 0.02);
 	PlanetInf plane9 = PlanetInf(QString("冥王星"), 49.3051216,
-            0, 0, 0.00212357040837083, 0.02);
+			0, 0, 0.00212357040837083, 0.02);
 #else /* 当在低性能arm平台运行时其精度会降低，需要刷新代码约束误差 */
-    PlanetInf plane1 = PlanetInf(QString("水星"), 0.466699373,
-            0, 0, 0.0224454181635083, 0.025);
-    PlanetInf plane2 = PlanetInf(QString("金星"), 0.728230404304,
-            0, 0, 0.0200920831078843, 0.04);
-    PlanetInf plane3 = PlanetInf(QString("地球"), 1.01669516395119,
-            0, 0, 0.0169192315430405, 0.05);
-    PlanetInf plane4 = PlanetInf(QString("火星"), 1.66600940556207,
-            0, 0, 0.0126911527706869, 0.1);
-    PlanetInf plane5 = PlanetInf(QString("木星"), 5.45889516,
-            0, 0, 0.00718117658914553, 0.2);
-    PlanetInf plane6 = PlanetInf(QString("土星"), 10.1240169,
-            0, 0, 0.00525205679561466, 0.5);
-    PlanetInf plane7 = PlanetInf(QString("天王星"), 20.1097686104,
-            0, 0, 0.0037464401867908, 1);
-    PlanetInf plane8 = PlanetInf(QString("海王星"), 30.33094746,
-            0, 0, 0.00311027848501303, 2);
-    PlanetInf plane9 = PlanetInf(QString("冥王星"), 49.3051216,
-            0, 0, 0.00212357040837083, 3);
+	PlanetInf plane1 = PlanetInf(QString("水星"), 0.466699373,
+			0, 0, 0.0224454181635083, 0.025);
+	PlanetInf plane2 = PlanetInf(QString("金星"), 0.728230404304,
+			0, 0, 0.0200920831078843, 0.04);
+	PlanetInf plane3 = PlanetInf(QString("地球"), 1.01669516395119,
+			0, 0, 0.0169192315430405, 0.05);
+	PlanetInf plane4 = PlanetInf(QString("火星"), 1.66600940556207,
+			0, 0, 0.0126911527706869, 0.1);
+	PlanetInf plane5 = PlanetInf(QString("木星"), 5.45889516,
+			0, 0, 0.00718117658914553, 0.2);
+	PlanetInf plane6 = PlanetInf(QString("土星"), 10.1240169,
+			0, 0, 0.00525205679561466, 0.5);
+	PlanetInf plane7 = PlanetInf(QString("天王星"), 20.1097686104,
+			0, 0, 0.0037464401867908, 1);
+	PlanetInf plane8 = PlanetInf(QString("海王星"), 30.33094746,
+			0, 0, 0.00311027848501303, 2);
+	PlanetInf plane9 = PlanetInf(QString("冥王星"), 49.3051216,
+			0, 0, 0.00212357040837083, 3);
 #endif
-    QVector<PlanetInf *> planes {&plane1,
-                &plane2, &plane3, &plane4,
-                &plane5, &plane6, &plane7,
-                &plane8, &plane9,
-    };
-    /* 当前绘制的曲线 */
-    PlanetInf *planeRun;
-    QTimer *Timer;
-    /* 绘制点与连线需要的变量 */
-    int px, py, pxOld = 0, pyOld = 0;
-    /* 绘制底层需要的对象 */
-    QPainter painter;
-    QPen pen;
-    QLineF line;
-    /* 绘制顶层所需要的对象 */
-    QPainter painterTop;
-    QPen penTop;
-    QPixmap pixmap = QPixmap(500, 500);
-    QPixmap pixmapTop = QPixmap(500, 500);
-    QColor colorTop;
+	QVector<PlanetInf *> planes {&plane1,
+				&plane2, &plane3, &plane4,
+				&plane5, &plane6, &plane7,
+				&plane8, &plane9,
+	};
+	/* 当前绘制的曲线 */
+	PlanetInf *planeRun;
+	QTimer *Timer;
+	/* 绘制点与连线需要的变量 */
+	int px, py, pxOld = 0, pyOld = 0;
+	int pxOlds[9];
+	int pyOlds[9];
+	/* 绘制底层需要的对象 */
+	QPainter painter;
+	QPen pen;
+	QLineF line;
+	/* 绘制顶层所需要的对象 */
+	QPainter painterTop;
+	QPen penTop;
+	QPixmap pixmap = QPixmap(500, 500);
+	QPixmap pixmapTop = QPixmap(500, 500);
+	QColor colorTop;
 };
 
 
